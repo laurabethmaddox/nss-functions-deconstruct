6cf37aa5cf0a65a6d1412d719b51154c8d99067c
Laura Beth Maddox <laurabethmaddox@gmail.com>
2022-01-13 15:19:09 -0600

    Functions deconstruct commit

---
+++ main.js
@@ -0,0 +1,21 @@
+const taskTwo = () => {
+    console.log("Pick up dirty clothes")
+}
+const taskOne = () => {
+    console.log("Pick up trash")
+}
+const taskThree = () => {
+    console.log("Put up clean clothes")
+}
+const taskFive = () => {
+    console.log("Dust Furniture")
+}
+const taskFour = () => {
+    console.log("Sort out clothes you don't want anymore")
+}
+
+taskOne()
+taskTwo()
+taskThree()
+taskFour()
+taskFive()
